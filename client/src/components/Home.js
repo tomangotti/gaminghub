52bfc956d994ca1ee9ca61aa0b4ecc5118156daa
thomas <tom.angotti11@gmail.com>
2023-03-15 15:58:47 -0600

    attempt know

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,7 @@
 import ChatFeed from "./ChatFeed"
 
 function Home({currentUser}){
-    const ws = new WebSocket("ws://gaminghub.onrender.com")
+    const ws = new WebSocket("wss://gaminghub.onrender.com")
 
     if( currentUser === null){
         return <h1>Please Log in</h1>
